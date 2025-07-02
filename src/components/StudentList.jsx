3d970e8614cc01fae8308bd65d4caad327d93b24
Aiyanna <20997291+aarcher711@users.noreply.github.com>
2025-07-02 18:19:36 -0400

    made some student files

---
+++ src/components/StudentList.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import StudentCard from "./StudentCards";
+
+const StudentList = ({ students, fetchAllStudents, navBarSetting }) => {
+  const filteredStudents = students.filter((task) => {
+    if (navBarSetting === "All Students") return true;
+    if (navBarSetting === "Enrolled Students") return student.enrolled;
+    if (navBarSetting === "Unenrolled Students") return !student.enrolled;
+  });
+  
+  
+  return (
+    <div>
+      {students.length > 0 ? (
+        students.map((student) => (
+          <StudentCard key={student.id} student={student} fetchAllStudents={fetchAllStudents} />
+        ))
+      ) : (
+        <p>No students found</p>
+      )}
+    </div>
+  );
+};
+
+export default StudentList;
