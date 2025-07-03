9f0ccca929b05336e580ec90f609c4f4bf934acb
Aiyanna <20997291+aarcher711@users.noreply.github.com>
2025-07-02 22:09:43 -0400

    pulling Mick's changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { createRoot } from "react-dom/client";
 import "./AppStyles.css";
 import NavBar from "./components/NavBar";
 import { BrowserRouter as Router, Routes, Link, Route } from "react-router";
 import Student from "./components/Student";
 import Campus from "./components/Campus";
+import StudentList from "./components/StudentList";
+//import CampusList from "./components/CampusList";
+
 
 const App = () => {
+  // new stuff play area begins
+  const [students, setStudents] = useState([]);
+
+  async function fetchAllStudents() {
+    try {
+      const response = await axios.get("http://localhost:8080/api/students");
+      setStudents(response.data);
+    } catch (error) {
+      console.error("Error fetching Students:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchAllStudents();
+  }, []);
+
+  // new stuff play area ends
   return (
     <div>
       <nav>
@@ -18,6 +38,27 @@ const App = () => {
         <img className="react-logo" src="/react-logo.svg" alt="React Logo" />
 
       </div>
+      <div>
+        <NavBar
+          navBarSetting={navBarSetting}
+          setNavBarSetting={setNavBarSetting}
+        />
+        {navBarSetting === "Add Student" && (
+          <AddTask
+            fetchAllStudents={fetchAllStudents}
+            setNavBarSetting={setNavBarSetting}
+          />
+        )}
+        {navBarSetting !== "Add Student" && (
+          <div className="app">
+            <StudentList
+              students={students}
+              fetchAllStudents={Students}
+              navBarSetting={navBarSetting}
+            />
+          </div>
+        )}
+            
       <Routes>
         <Route path="/Student" element={<Student />} />
         <Route path="/Campus" element={<Campus />} />
